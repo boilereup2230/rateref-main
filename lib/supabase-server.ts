ac4bf3018fd9a03ae5c23df1d5ddcfe5d38eb059
boilereup2230 <boilerup2230@gmail.com>
2026-05-22 17:15:32 -0400

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -4,8 +4,8 @@ import { cookies } from 'next/headers'
 export async function createServerSupabaseClient() {
   const cookieStore = await cookies()
   return createServerClient(
-    "https://mnrstfenfqvrohcjrwx.supabase.co",
-    "sb_publishable_PEimt--Ur3iMRHHX-8tUOw_S5Q0ASST",
+    "https://vgquclxeehmqqxqcpvie.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZncXVjbHhlZWhtcXF4cWNwdmllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NzA3NzUsImV4cCI6MjA5NTA0Njc3NX0.j9RosPQGfbbM1fizQArx8Qv7y2VUBVZsoxYTn_BRmBk",
     {
       cookies: {
         getAll() { return cookieStore.getAll() },
