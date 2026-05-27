4a4cdf3fbc00c6a631d10c95fdf68644d1f408b3
boilereup2230 <boilerup2230@gmail.com>
2026-05-27 10:18:50 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,31 @@
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  return NextResponse.next()
+export async function middleware(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({ request })
+
+  const supabase = createServerClient(
+    "https://vgquclxeehmqqxqcpvie.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZncXVjbHhlZWhtcXF4cWNwdmllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NzA3NzUsImV4cCI6MjA5NTA0Njc3NX0.j9RosPQGfbbM1fizQArx8Qv7y2VUBVZsoxYTn_BRmBk",
+    {
+      cookies: {
+        getAll() { return request.cookies.getAll() },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+          supabaseResponse = NextResponse.next({ request })
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          )
+        },
+      },
+    }
+  )
+
+  await supabase.auth.getUser()
+
+  return supabaseResponse
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/setup/:path*'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 }
